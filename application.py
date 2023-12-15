4622a437908dcd3406542cf638bb01c9e730c50d
Mathwebb <matheus.webber@hotmail.com>
2023-12-15 16:42:30 -0300

    feat(djpeg-evaluation): Adding a script to allow the evaluation of the DJPEG model using our own built datasets

--- application.py
+++ application.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 import math
 from PIL import JpegImagePlugin
 from PIL import Image
+import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
@@ -24,7 +25,7 @@ def _extract_patches(Y, patch_size, stride):
     return patches, H, W
 
 
-def localizing_double_JPEG(Y, qvectors):
+def localizing_double_JPEG(Y, qvectors, stride=32, batch_size=32, net=None, device=None):
     net.eval()
     result = 0
     PATCH_SIZE = 256
@@ -36,23 +37,23 @@ def localizing_double_JPEG(Y, qvectors):
     # result = np.zeros_like(Y)
 
     patches, H, W = _extract_patches(
-        Y, patch_size=PATCH_SIZE, stride=args.stride)
+        Y, patch_size=PATCH_SIZE, stride=stride)
     result = np.zeros((H, W))
     print('Number of patches: {}'.format(len(patches)))
     print('Number of batches: {}'.format(
-        math.ceil(len(patches) / args.batch_size)))
+        math.ceil(len(patches) / batch_size)))
     print('Patch size: {}'.format(PATCH_SIZE))
 
     # import pdb; pdb.set_trace()
-    num_batches = math.ceil(len(patches) / args.batch_size)
+    num_batches = math.ceil(len(patches) / batch_size)
 
     result_flatten = np.zeros((H*W))
     for i in range(num_batches):
         print('[{} / {}] Detecting...'.format(i, num_batches))
         if i == (num_batches-1):  # last batch
-            batch_Y = patches[i*args.batch_size:]
+            batch_Y = patches[i*batch_size:]
         else:
-            batch_Y = patches[i*args.batch_size:(i+1)*args.batch_size]
+            batch_Y = patches[i*batch_size:(i+1)*batch_size]
 
         batch_size = len(batch_Y)
         batch_Y = np.array(batch_Y)
@@ -62,7 +63,7 @@ def localizing_double_JPEG(Y, qvectors):
         batch_output = net(batch_Y, batch_qvectors)
         batch_output = F.softmax(batch_output, dim=1)
 
-        result_flatten[(i*args.batch_size):(i*args.batch_size)+batch_size] = \
+        result_flatten[(i*batch_size):(i*batch_size)+batch_size] = \
             batch_output.detach().cpu().numpy()[:, 0]
 
     result = np.reshape(result_flatten, (H, W))
@@ -70,18 +71,12 @@ def localizing_double_JPEG(Y, qvectors):
     return result
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--stride', type=int, default=32)
-    parser.add_argument('--batch_size', type=int, default=32)
-    parser.add_argument('--target', type=str, default='splicing.jpg')
-    args = parser.parse_args()
-
-    dir_name = './images'
-    file_name = args.target
-    result_name = file_name.split('.')[0] + '_result.jpg'
-    file_path = os.path.join(dir_name, file_name)
-    result_path = os.path.join(dir_name, result_name)
+def djpeg(file_path: str, stride=32, batch_size=32):
+    result_name = file_path.split('\\')[-1]
+    result_name = result_name[:result_name.rfind('.')] + '_result.jpg'
+    result_path = file_path.replace(
+        file_path.split('\\')[-1], 'results')
+    result_path = os.path.join(result_path, result_name)
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print(device)
@@ -102,23 +97,59 @@ if __name__ == "__main__":
     net.to(device)
 
     # localizaing using trained detecting double JPEG network.
-    result = localizing_double_JPEG(Y, qvector)
+    result = localizing_double_JPEG(
+        Y, qvector, stride=stride, batch_size=batch_size, net=net, device=device)
     print(result.shape)
 
-    # plot and save the result
-    fig = plt.figure()
-    columns = 2
-    rows = 1
-    fig.add_subplot(rows, columns, 1)
-    plt.imshow(Image.open(file_path))
-    plt.title('input')
-
-    fig.add_subplot(rows, columns, 2)
+    # Save the result
     result = result*255
     result = result.astype('uint8')
     img_result = Image.fromarray(result)
+    img_result = img_result.resize((Y.shape[1], Y.shape[0]), Image.NEAREST)
     img_result.convert("L")
-    plt.imshow(img_result, cmap='gray', vmin=0, vmax=255)
-    plt.title('result')
-    plt.savefig(result_path)
-    plt.show()
+    img_result.save(result_path)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--stride', type=int, default=32)
+    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--target', type=str, default='splicing.jpg')
+    args = parser.parse_args()
+
+    dir_name = './images'
+    file_name = args.target
+    result_name = file_name.split('.')[0] + '_result.jpg'
+    file_path = os.path.join(dir_name, file_name)
+    result_path = os.path.join(dir_name, 'results', result_name)
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(device)
+
+    # read an image
+    # img = np.asarray(Image.open(file_path))
+    im = Image.open(file_path)
+    im = im.convert('YCbCr')
+    Y = np.array(im)[:, :, 0]
+
+    # read quantization table of Y channel from jpeg images
+    qvector = read_q_table(file_path).flatten()
+
+    # load pre-trained weights
+    net = Djpegnet(device)
+    net.load_state_dict(torch.load(
+        './model/djpegnet.pth', map_location=device))
+    net.to(device)
+
+    # localizaing using trained detecting double JPEG network.
+    result = localizing_double_JPEG(
+        Y, qvector, stride=args.stride, batch_size=args.batch_size, net=net, device=device)
+    print(result.shape)
+
+    # Save the result
+    result = result*255
+    result = result.astype('uint8')
+    img_result = Image.fromarray(result)
+    img_result = img_result.resize((Y.shape[1], Y.shape[0]), Image.NEAREST)
+    img_result.convert("L")
+    img_result.save(result_path)
---
+++ evaluate.py
@@ -0,0 +1,88 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from application import djpeg
+
+ORIGINAL_DIR = 'D:\\data_sets\\oral_photograph\\roboflow\\proyecto_odoto_v1.v6i.coco-segmentation\\train'
+TAMPERED_DIR = 'C:\\Repositories\\iniciacao-cientifica\\cereia\\tampering-detection\\data_sets\\oral_tampering'
+
+
+def create_jpeg_masks(original_dir: str, tampered_dir: str):
+    if not os.path.exists(os.path.join(original_dir, 'results')):
+        os.mkdir(os.path.join(original_dir, 'results'))
+
+    for file_name in os.listdir(original_dir):
+        if file_name.endswith('.jpg') and not file_name.endswith('_result.jpg'):
+            djpeg(os.path.join(original_dir, file_name))
+
+    if not os.path.exists(os.path.join(tampered_dir, 'results')):
+        os.mkdir(os.path.join(tampered_dir, 'results'))
+
+    for file_name in os.listdir(tampered_dir):
+        if file_name.endswith('.jpg') and not file_name.endswith('_result.jpg'):
+            djpeg(os.path.join(tampered_dir, file_name))
+
+
+def predict(image_path: str) -> str:
+    image = cv.imread(image_path, cv.IMREAD_GRAYSCALE)
+    hist = cv.calcHist([image], [0], None, [256], (0, 255))
+    count = 0
+    cumulative_hist = []
+    for intensity in hist:
+        count += intensity[0]
+        cumulative_hist.append(count)
+
+    if cumulative_hist[127] >= 0.5 * cumulative_hist[255]:
+        return 'tampered'
+    else:
+        return 'original'
+
+
+def evaluate(original_results_dir: str, tampered_results_dir: str) -> dict:
+    metrics: dict[str, float] = {
+        'true_positives': 0,
+        'true_negatives': 0,
+        'false_positives': 0,
+        'false_negatives': 0
+    }
+    for file_name in os.listdir(original_results_dir):
+        file_path = os.path.join(original_results_dir, file_name)
+        if predict(file_path) == 'original':
+            metrics['true_negatives'] += 1
+        else:
+            metrics['false_negatives'] += 1
+
+    for file_name in os.listdir(tampered_results_dir):
+        file_path = os.path.join(tampered_results_dir, file_name)
+        if predict(os.path.join(file_path)) == 'tampered':
+            metrics['true_positives'] += 1
+        else:
+            metrics['false_positives'] += 1
+
+    for metric in metrics.values():
+        print(metric)
+
+    metrics['accuracy'] = (metrics['true_positives'] +
+                           metrics['true_negatives']) / (metrics['true_positives'] +
+                                                         metrics['true_negatives'] +
+                                                         metrics['false_positives'] +
+                                                         metrics['false_negatives'])
+    metrics['precision'] = metrics['true_positives'] / \
+        (metrics['true_positives'] + metrics['false_positives'])
+    metrics['recall'] = metrics['true_positives'] / \
+        (metrics['true_positives'] + metrics['false_negatives'])
+    return metrics
+
+
+if __name__ == '__main__':
+    # create_jpeg_masks(ORIGINAL_DIR, TAMPERED_DIR)
+
+    original_results = [file_name for file_name in os.listdir(
+        os.path.join(ORIGINAL_DIR, 'results')) if file_name.endswith('_result.jpg')]
+    tampered_results = [file_name for file_name in os.listdir(
+        TAMPERED_DIR) if file_name.endswith('_result.jpg')]
+
+    metrics = evaluate(os.path.join(ORIGINAL_DIR, 'results'),
+                       os.path.join(TAMPERED_DIR, 'results'))
+    print(metrics)
